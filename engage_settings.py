b5dfac2e9f2bba6932e0e35171c81ef670aa851e
Edan Krolewicz <ekrolewicz6@gmail.com>
2018-03-13 15:56:52 -0700

    Update engage_settings.py

--- engage_settings.py
+++ engage_settings.py
@@ -4,11 +4,12 @@ from __future__ import print_function
 from time import localtime, strftime
 import twitter
 import random
+import os
 
-CONSUMER_KEY = '$CONSUMER_KEY'
-CONSUMER_SECRET = '$CONSUMER_SECRET'
-ACCESS_TOKEN = '$ACCESS_TOKEN'
-ACCESS_TOKEN_SECRET = '$ACCESS_TOKEN_SECRET'
+CONSUMER_KEY = os.environ($CONSUMER_KEY)
+CONSUMER_SECRET = os.environ($CONSUMER_SECRET)
+ACCESS_TOKEN = os.environ($ACCESS_TOKEN)
+ACCESS_TOKEN_SECRET = os.environ($ACCESS_TOKEN_SECRET)
 
 
 # Create an Api instance.
