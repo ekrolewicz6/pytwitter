46be1b6134cbc0d5bbc5672d0c366bfef8e608f7
Edan Krolewicz <ekrolewicz6@gmail.com>
2018-03-13 15:57:46 -0700

    Update engage_settings_chakra.py

--- engage_settings_chakra.py
+++ engage_settings_chakra.py
@@ -6,10 +6,10 @@ import twitter
 import random
 import os
 
-CONSUMER_KEY = os.environ($CONSUMER_KEY)
-CONSUMER_SECRET = os.environ($CONSUMER_SECRET)
-ACCESS_TOKEN = os.environ($ACCESS_TOKEN)
-ACCESS_TOKEN_SECRET = os.environ($ACCESS_TOKEN_SECRET)
+CONSUMER_KEY = os.environ(CONSUMER_KEY)
+CONSUMER_SECRET = os.environ(CONSUMER_SECRET)
+ACCESS_TOKEN = os.environ(ACCESS_TOKEN)
+ACCESS_TOKEN_SECRET = os.environ(ACCESS_TOKEN_SECRET)
 
 
 # Create an Api instance.
